2cf893dce1fee2d74cb7c3328b1298fd87605dbe
Wesley-Oliveira <wesley0leby@gmail.com>
2021-07-03 18:58:27 -0300

    Saving on S3

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -3,8 +3,11 @@ import path from "path";
 import handlebars from "handlebars";
 import fs from "fs";
 import dayjs from "dayjs";
-
+import { S3 } from "aws-sdk";
 import { document } from "../utils/dynamodbClient";
+
+require('dotenv').config();
+
 
 interface ICreateCertificate {
     id: string;
@@ -76,10 +79,20 @@ export const handle = async (event) => {
     await browser.close();
 
     // Save on S3
+    const s3 = new S3();
+    await s3.putObject({
+        Bucket: process.env.BUCKET,
+        Key: `${id}.pdf`,
+        ACL: "public-read",
+        Body: pdf,
+        ContentType: "application/pdf",
+    }).promise();
+
     return {
         statusCode: 201,
         body: JSON.stringify({
             message: "Certificate created!",
+            url: `${process.env.URL_CERTIFICATE}${id}.pdf`
         }),
         headers: {
             "Content-type": "application/json"
