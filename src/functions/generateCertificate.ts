f125a0fa87986f13f2bd76a8248f30ae811bb8a1
Wesley-Oliveira <wesley0leby@gmail.com>
2021-07-03 18:00:52 -0300

    Creating template with handlebars

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -1,3 +1,9 @@
+import chromium from "chrome-aws-lambda";
+import path from "path";
+import handlebars from "handlebars";
+import fs from "fs";
+import dayjs from "dayjs";
+
 import { document } from "../utils/dynamodbClient";
 
 interface ICreateCertificate {
@@ -6,9 +12,24 @@ interface ICreateCertificate {
     grade: string;
 }
 
+interface ITemplate {
+    id: string;
+    name: string;
+    grade: string;
+    date: string;
+    medal: string;
+};
+
+const compile = async function( data: ITemplate ) {
+    const filePath = path.join(process.cwd(), "src", "templates", "certificate.hbs");
+    const html = fs.readFileSync(filePath, "utf-8");
+    return handlebars.compile(html)(data);
+}
+
 export const handle = async (event) => {
     const { id, name, grade } = JSON.parse(event.body) as ICreateCertificate;
 
+    // Insert on db
     await document.put({
         TableName: "users_certificates",
         Item: {
@@ -17,6 +38,21 @@ export const handle = async (event) => {
             grade,
         }
     }).promise();
+
+    const medalPath = path.join(process.cwd(), "src", "templates", "selo.png");
+    const medal = fs.readFileSync(medalPath, "base64");
+
+    const data: ITemplate {
+        date: dayjs().format("DD//MM/YYYY"),
+        grade,
+        name,
+        id,
+        medal
+    }
+
+    // Generate certificate
+    // Compiling using handlebars -> Transform on PDF -> Save on S3
+    const content = await compile(data);
 
     return {
         statusCode: 201,
